c5821ba869bf466032fce5dfd97e83e3084c1eab
leomarqueslimait-jpg <leomarqueslimait@gmail.com>
2026-07-20 21:31:06 -0500

    upload

--- form/student.py
+++ form/student.py
@@ -124,35 +124,3 @@ class Student:
         enrollment_date = input("What is the enrollment date? (MM/DD/YYYY): ")
         school_site = input("What is the school site?: ")
         return cls(first_name, last_name, dob, guardian_name, guardian_email, guardian_phone, enrollment_date, school_site)
-
-
-FIELDNAMES = ["first_name", "last_name", "dob", "guardian_name",
-              "guardian_email", "guardian_phone", "enrollment_date", "school_site"]
-
-def main():
-    student = Student.get()
-    file_exists = False
-    try:
-        with open("student.csv", "r"):
-            file_exists = True
-    except FileNotFoundError:
-        pass
-
-    with open("student.csv", "a", newline="") as file:
-            writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
-            if not file_exists:
-                writer.writeheader()
-
-            writer.writerow({
-            "first_name": student.first_name,
-            "last_name": student.last_name,
-            "dob": student.dob,
-            "guardian_name": student.guardian_name,
-            "guardian_email": student.guardian_email,
-            "guardian_phone": student.guardian_phone,
-            "enrollment_date": student.enrollment_date,
-            "school_site": student.school_site
-        })
-
-if __name__ == "__main__":
-    main()
